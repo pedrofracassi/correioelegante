ca2cc810a75bff86b9a93f4f54456cff36541a6c
Pedro Fracassi <pedroluizfracassi@gmail.com>
2020-05-31 03:31:45 -0300

    add warning

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -4,6 +4,9 @@ import SubmissionForm from './SubmissionForm.jsx'
 export default function Home() {
   return (
     <div>
+      <center>
+        <h1>ISSO AQUI É UM PROTÓTIPO FAVOR NÃO ENVIAR CARTINHAS</h1>
+      </center>
       <SubmissionForm />
     </div>
   )
